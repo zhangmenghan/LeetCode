1b767d3af5767f51620ad44b3d0e09dda55d9259
zhangmenghan <2423306508@qq.com>
2019-09-16 11:33:52 +0800

    2019091601

--- src/main/java/com/simple/S_733.java
+++ src/main/java/com/simple/S_733.java
@@ -1,17 +1,34 @@
 package com.simple;
 
+import java.util.Queue;
+import java.util.LinkedList;
+
 public class S_733 {
 
     public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
-        int row = image.length;
-        int col = image[0].length;
-        int[][] result = new int[row][col];
+        int[] directions = new int[]{0, 1, 0, -1, 0};
+        int m = image.length;
+        int n = image[0].length;
+        int originalValue = image[sr][sc];
+        image[sr][sc] = newColor;
 
-        return result;
-    }
+        boolean[][] visited = new boolean[m][n];
 
-    public static void main(String[] args) {
-        S_733 s_733 = new S_733();
-        System.out.println();
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{sr, sc});
+        while (!queue.isEmpty()) {
+            int[] curr = queue.poll();
+            visited[curr[0]][curr[1]] = true;
+            for (int i = 0; i < directions.length - 1; i++) {
+                int nextR = curr[0] + directions[i];
+                int nextC = curr[1] + directions[i + 1];
+                if (nextR < 0 || nextC < 0 || nextR >= m || nextC >= n || image[nextR][nextC] != originalValue || visited[nextR][nextC]) {
+                    continue;
+                }
+                image[nextR][nextC] = newColor;
+                queue.offer(new int[]{nextR, nextC});
+            }
+        }
+        return image;
     }
 }
